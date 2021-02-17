9748c777586844629ab608bfbc9817c131bc540a
Ayesha Zahid <ayeshazahid758@gmail.com>
2021-02-17 22:14:27 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
-
-<?php
-$f=2;
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AyeshaZahid_Ass02(a)</title>
+    <title>AyeshaZahid_Ass02</title>
 </head>
 <body>
-    <h1> Table Of Two</h1>
-
+<h4><b>Table Generator</b></h4>
+    <form method="POST">
+        <input type="text" name="A">
+        <input type="submit" value="submit" name="submit">
+    </form>
 
     <?php
 
-$f=2;
-for  ($i=1; $i<=10; $i++){
-    
-    echo  $f, " * ", $i, "=",  $f*$i, "<br>";
-    
+if (isset($_POST["submit"])){
+    $F=$_POST["A"];
+    for ($i=1; $i<=10; $i++){ 
+        echo "<table style='border: 1px solid #555;' >
+        <tr>
+        <td>".$F."</td>
+        <td>*</td>
+        <td>".$i."</td>
+        <td>=</td>
+        <td>".$F * $i."</td>
+        </tr>
+        </table>";
+        // echo  $F, "*" , $i, "=", $F * $i,"<br>" ;
+    }
 }
-?>  
-<br>
-
-
+?>
 </body>
 </html>
